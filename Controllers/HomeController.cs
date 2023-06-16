a24ce21a2f2f04cfdd92726c0a283c8f6bb3bd2e
MohammadAminJafer <amin.jafer@n3o.ltd>
2023-06-16 16:03:33 +0500

    Anoteher commit to check if after disabling 'Build pull requests from forks of this repository' the Prechecks still run or not and is build triggered?

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,6 +15,7 @@ namespace pipelines_dotnet_core.Controllers
             //Just some random comments to check the Git Hub stuff
             return View();
             //some more changes just to check if it is working. And if it is good.
+            //Making a branch to commit the changes beoifre merging.
         }
 
         public IActionResult Privacy()
